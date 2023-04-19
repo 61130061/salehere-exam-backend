94f2582fe370048aff806c868fcd14df897acc7b
61130061 <thezeroyou@gmail.com>
2023-04-19 14:41:02 +0700

    ➕ add CRUD function

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ var schema = buildSchema(`
     id: ID!
     text: String!
     sender: User!
-    timestamp: String!
+    time: String!
   }
   
   type ChatRoom {
@@ -26,13 +26,16 @@ var schema = buildSchema(`
   
   type Query {
     hello: String
+    getUserById(userId: ID!): User!
     chatRooms: [ChatRoom!]!
-    chatRoom(name: String!): ChatRoom!
+    chatRoomByName(name: String!): ChatRoom!
+    chatRoomById(id: ID!): ChatRoom!
   }
   
   type Mutation {
     createChatRoom(name: String!): ChatRoom!
     sendMessage(roomId: ID!, senderId: ID!, text: String!): Message!
+    user(name: String!): User!
   }
   
   schema {
@@ -41,7 +44,6 @@ var schema = buildSchema(`
   }
 `)
 
-let messages = [];
 let chatRooms = [];
 let users = [];
 
@@ -50,11 +52,64 @@ var root = {
   hello: () => {
     return crypto.randomUUID()
   },
+  sendMessage: ({ roomId, senderId, text }) => {
+    const sender = users.filter(item => item.id === senderId);
+
+    if (sender.length < 1) throw new Error('Sender invalidated');
+
+    const newMessage = {
+      id: crypto.randomUUID(),
+      sender: sender[0],
+      text,
+      time: new Date()
+    }
+
+    const existRoom = chatRooms.filter(item => item.id === roomId);
+    if (existRoom.length < 1) throw new Error('Chatroom not found');
+
+    chatRooms.forEach((d) => {
+      if (d.id === roomId) {
+        d.messages.push(newMessage);
+        return d
+      }
+    });
+
+    return newMessage;
+  },
+  getUserById: ({ userId }) => {
+    const user = users.filter(item => item.id === id);
+
+    if (user.length < 1) throw new Error('User not found');
+
+    return user[0]
+  },
+  user: ({ name }) => {
+    const user = users.filter(item => item.name === name);
+
+    if (user.length > 0) return user[0]
+    else {
+      const newUser = {
+        id: crypto.randomUUID(),
+        name
+      }
+
+      users.push(newUser);
+
+      return newUser
+    }
+  },
   chatRooms: () => {
     return chatRooms
   },
-  chatRoom: ({ name }) => {
+  chatRoomByName: ({ name }) => {
     const results = chatRooms.filter(item => item.name === name);
+
+    if (results.length < 1) throw new Error('Chatroom not found');
+
+    return results[0]
+  },
+  chatRoomById: ({ id }) => {
+    const results = chatRooms.filter(item => item.id === id);
 
     if (results.length < 1) throw new Error('Chatroom not found');
 
